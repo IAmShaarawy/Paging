45fd9002c4ea09c31740849c0a2a4d532272d9e4
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-10-13 17:03:10 +0200

    feat: make file called views extensions and make an infix extension function to image view class to load images as rounded

---
+++ app/src/main/java/elshaarawyx/pagging/ViewsExtensions.kt
@@ -0,0 +1,15 @@
+package elshaarawyx.pagging
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+/**
+ * Created by elshaarawy on 10/13/18.
+ */
+infix fun ImageView.loadAsRounded(url: String) {
+    Glide.with(this)
+            .load(url)
+            .apply(RequestOptions.circleCropTransform())
+            .into(this)
+}
