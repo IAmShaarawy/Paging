9d24de424b51a4c9468aefa937be5a6081ab0868
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-10-14 01:03:52 +0200

    feat: add an implementation for GithubUsersPageKeyedDataSource

---
+++ app/src/main/java/elshaarawyx/pagging/GithubUsersPageKeyedDataSource.kt
@@ -0,0 +1,51 @@
+package elshaarawyx.pagging
+
+import android.arch.paging.PageKeyedDataSource
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
+
+/**
+ * Created by elshaarawy on 10/14/18.
+ */
+class GithubUsersPageKeyedDataSource : PageKeyedDataSource<Long, UserEntity>() {
+
+    override fun loadInitial(params: LoadInitialParams<Long>, callback: LoadInitialCallback<Long, UserEntity>) {
+        loadData(0) {
+            callback.onResult(it, null, it.lastOrNull()?.id)
+        }
+    }
+
+    override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Long, UserEntity>) {
+        params.key?.let {
+            loadData(it) { result ->
+                callback.onResult(result, result.lastOrNull()?.id)
+            }
+        }
+    }
+
+    override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Long, UserEntity>) {
+        params.key?.let {
+            loadData(it) { result ->
+                callback.onResult(result, result.firstOrNull()?.id)
+            }
+        }
+    }
+
+    private inline fun loadData(since: Long, crossinline onSuccess: (List<UserEntity>) -> Unit) {
+        GlobalScope.launch {
+
+            val response = GithubAPIsFactory()
+                    .create(UsersAPIs::class.java)
+                    .retrieveUsers(since)
+                    .await()
+
+            GlobalScope.launch(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    response.body()
+                            ?.also(onSuccess)
+                }
+            }
+        }
+    }
+}
